ac678dd29acc5f85af363e8055c665112f328442
Mohammed_maktap_Noor <mohammedalyfouad@gmail.com>
2026-08-18 11:55:38 +0300

    Building CSS With build.rs to global.css

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    println!("cargo:rerun-if-changed=static/css");
+
+    let files = [
+        "static/css/base/reset.css",
+        "static/css/components/buttons.css",
+        "static/css/components/cards.css",
+        "static/css/components/forms.css",
+        "static/css/components/modals.css",
+        "static/css/components/search.css",
+        "static/css/components/toasts.css",
+        "static/css/components/toolbars.css",
+        "static/css/layouts/sidebar.css",
+    ];
+
+    let mut merged = String::new();
+    for file in files {
+        let content = fs::read_to_string(file)
+            .unwrap_or_else(|e| panic!("failed to read {file}: {e}"));
+        merged.push_str(&format!("/* --- {file} --- */\n"));
+        merged.push_str(&content);
+        merged.push('\n');
+    }
+
+    fs::write("static/css/global.css", merged)
+        .expect("failed to write global.css");
+}
